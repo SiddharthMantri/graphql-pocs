e1628c42ddf927eb570bd0420b5817aa5c6937be
Siddharth Mantri <siddharthmantri1@gmail.com>
2022-05-09 19:52:40 +0200

    refactor: fix h1 label

--- client/app.tsx
+++ client/app.tsx
@@ -6,10 +6,7 @@ import UserList from "./components/UserList";
 
 const App = (): JSX.Element => (
   <ApolloProvider client={apolloClient}>
-    <h1>
-      Welcome to React App connects to Apollo Gateway and Subscriptions
-      separately
-    </h1>
+    <h1>React App connects to Apollo Gateway and Subscriptions separately</h1>
     <h3>... but renders them together</h3>
     <h4>Rendering Books through gateway</h4>
     <BookList />
